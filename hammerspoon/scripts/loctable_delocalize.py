416e5468d9188aba14b30d1539979065556dc684
Yiwen Xu <xywthu16@163.com>
2024-01-14 02:57:45 +0800

    remove spaces

--- hammerspoon/scripts/loctable_delocalize.py
+++ hammerspoon/scripts/loctable_delocalize.py
@@ -7,7 +7,7 @@ loctable, string, lang = sys.argv[1:4]
 
 with open(loctable, 'rb') as fp:
   data = plistlib.load(fp)
-  
+
 try:
   key = list(data[lang].keys())[list(data[lang].values()).index(string)]
 except ValueError:
