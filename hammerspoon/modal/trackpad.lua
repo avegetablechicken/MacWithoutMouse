6877affc6c681f3eaa53ab9580bc828b10d490e6
Yiwen Xu <xywthu16@163.com>
2024-07-27 02:34:38 +0800

    trackpad hotkeys support being suspended

--- hammerspoon/modal/trackpad.lua
+++ hammerspoon/modal/trackpad.lua
@@ -14,10 +14,10 @@ This.CORNER_SIZE = params.trackpadCornerSize or 0.25
 if This.CORNER_SIZE <= 0 then This.CORNER_SIZE = 0.1 end
 if This.CORNER_SIZE >= 0.5 then This.CORNER_SIZE = 0.4 end
 
-function This.bind(mods, key, message, pressedfn, releasedfn, repeatfn)
+local function getModsCode(mods)
   local modsCode = 0
   if type(mods) == 'table' then
-    for i=#mods,1,-1 do
+    for i = #mods, 1, -1 do
       if mods[i] == "top-left" then
         modsCode = modsCode + 1
         table.remove(mods, i)
@@ -62,17 +62,19 @@ function This.bind(mods, key, message, pressedfn, releasedfn, repeatfn)
       mods = string.gsub(mods, "⌟", "")
     end
   end
-  if This.keys[modsCode] == nil then
-    This.keys[modsCode] = {}
-  end
-  local hotkey = newHotkeyImpl(mods, key, message, pressedfn, releasedfn, repeatfn)
+  return modsCode
+end
+
+local function getModsRepr(modsCode)
   local modsRepr = ""
   if (modsCode // 8) % 2 == 1 then modsRepr = trackpad["bottom-right"] end
   if (modsCode // 4) % 2 == 1 then modsRepr = trackpad["bottom-left"] .. modsRepr end
   if (modsCode // 2) % 2 == 1 then modsRepr = trackpad["top-right"] .. modsRepr end
   if modsCode % 2 == 1 then modsRepr = trackpad["top-left"] .. modsRepr end
-  hotkey.msg = modsRepr .. hotkey.msg
-  table.insert(This.keys[modsCode], hotkey)
+  return modsRepr
+end
+
+function This._startEventTapper()
   if This.eventtapper == nil then
     This.eventtapper = hs.eventtap.new({hs.eventtap.event.types.gesture},
     function(ev)
@@ -111,6 +113,31 @@ function This.bind(mods, key, message, pressedfn, releasedfn, repeatfn)
       return false
     end):start()
   end
+end
+
+function This.bindNoSuspend(mods, ...)
+  local modsCode = getModsCode(mods)
+  if This.keys[modsCode] == nil then
+    This.keys[modsCode] = {}
+  end
+  local hotkey = newHotkeyImpl(mods, ...)
+  local modsRepr = getModsRepr(modsCode)
+  hotkey.msg = modsRepr .. hotkey.msg
+  table.insert(This.keys[modsCode], hotkey)
+  This._startEventTapper()
+  return hotkey
+end
+
+function This.bind(mods, ...)
+  local modsCode = getModsCode(mods)
+  if This.keys[modsCode] == nil then
+    This.keys[modsCode] = {}
+  end
+  local hotkey = newHotkey(mods, ...)
+  local modsRepr = getModsRepr(modsCode)
+  hotkey.msg = modsRepr .. hotkey.msg
+  table.insert(This.keys[modsCode], hotkey)
+  This._startEventTapper()
   return hotkey
 end
 
