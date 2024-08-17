d4fe0206afa409c8a47c8c467c6982bbd21fcd6a
Yiwen Xu <xywthu16@163.com>
2024-08-17 11:21:51 +0800

    use "gtimeout" to kill commandline process which lasts too long in "checkinlab.sh"

--- hammerspoon/scripts/checkinlab.sh
+++ hammerspoon/scripts/checkinlab.sh
@@ -1,8 +1,12 @@
 #! /bin/zsh
 
-interface=$(route get default | grep interface | awk '{print $2}')
+if [ -x "$(which gtimeout 2>/dev/null)" ]; then
+  interface=$(gtimeout 1s route get default | grep interface | awk '{print $2}')
+else
+  interface=$(route get default | grep interface | awk '{print $2}')
+fi
 if [[ $? -ne 0 ]]; then
-	return 1
+	return -1
 fi
 
 networkservice=$(networksetup -listallhardwareports | awk "/${interface}/ {print prev} {prev=\$0;}" | awk -F: '{print $2}' | awk '{$1=$1};1')
