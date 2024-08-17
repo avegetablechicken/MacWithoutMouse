81589556f9ae6177d90950c2b7ba5deb0740faa8
Yiwen Xu <xywthu16@163.com>
2024-08-17 11:22:12 +0800

    fix typo

--- hammerspoon/scripts/checkinlab.sh
+++ hammerspoon/scripts/checkinlab.sh
@@ -12,7 +12,7 @@ fi
 networkservice=$(networksetup -listallhardwareports | awk "/${interface}/ {print prev} {prev=\$0;}" | awk -F: '{print $2}' | awk '{$1=$1};1')
 
 if [[ "Wi-Fi" = "$networkservice" ]]; then
-	/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep -o 'SSID: .*$' | cut -c 7- | grep -q "<possible-ssid-pattern"
+	/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep -o 'SSID: .*$' | cut -c 7- | grep -q "<possible-ssid-pattern>"
 elif [[ "$networkservice" =~ "^USB (.*) LAN$" ]]; then
 	ip=$(ifconfig "$interface" | grep "inet[^6]" | awk '{print $2}')
 	[[ "$ip" =~ "<possible-ip-pattern>" ]] && return 0 || return 1
