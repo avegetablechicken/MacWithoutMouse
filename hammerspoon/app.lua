4e12b717ebf1acfe1e8948ed9be8b9f72427d88b
Yiwen Xu <xywthu16@163.com>
2024-11-27 02:18:00 +0800

    add ctrl+r to open recent in `Screen Sharing`

--- hammerspoon/app.lua
+++ hammerspoon/app.lua
@@ -2515,6 +2515,20 @@ appHotKeyCallbacks = {
     ["closeWindow"] = specialCommonHotkeyConfigs["closeWindow"]
   },
 
+  ["com.apple.ScreenSharing"] =
+  {
+    ["openRecent"] = {
+      message = localizedMessage("Open Recent"),
+      condition = checkMenuItem({ "Connect", "Open Recent" }),
+      fn = function(menuItemPath, appObject)
+        showMenuItemWrapper(function()
+          appObject:selectMenuItem({ menuItemPath[1] })
+          appObject:selectMenuItem(menuItemPath)
+        end)()
+      end
+    },
+  },
+
   ["com.parallels.desktop.console"] =
   {
     ["new..."] = {
