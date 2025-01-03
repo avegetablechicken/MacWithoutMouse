526d9d703b721c5383fdbffb78d2238386ff3560
Yiwen Xu <xywthu16@163.com>
2025-01-04 03:33:27 +0800

    use "hs.axuielement" instead of applescript to query menu item of right menu bar of `V2rayU`

--- hammerspoon/system.lua
+++ hammerspoon/system.lua
@@ -182,66 +182,66 @@ end
 -- toggle connect/disconnect VPN using `V2rayU`
 local function toggleV2RayU(enable, alert)
   local bundleID = proxyAppBundleIDs.V2rayU
-
-  local script = ""
   if findApplication(bundleID) == nil then
-    script = [[
-      tell application id "]] .. bundleID .. [[" to activate
-    ]]
+    hs.application.launchOrFocusByBundleID(bundleID)
   end
 
-  script = script .. [[
-    tell application "System Events"
-      set popupMenu to menu 1 of menu bar item 1 of last menu bar of ¬
-          (first application process whose bundle identifier is "]] .. bundleID .. [[")
-      %s
-    end tell
-
-    return ret
-  ]]
-
-  if enable == true then
-    script = string.format(script, [[
-      if exists menu item "Turn v2ray-core On" of popupMenu then
-        click menu item "Turn v2ray-core On" of popupMenu
-      end if
-      set ret to 0
-    ]])
-  elseif enable == false then
-    script = string.format(script, [[
-      if exists menu item "Turn v2ray-core Off" of popupMenu then
-        click menu item "Turn v2ray-core Off" of popupMenu
-      end if
-      set ret to 1
-    ]])
-  else
-    script = string.format(script, [[
-      if exists menu item "Turn v2ray-core On" of popupMenu then
-        click menu item "Turn v2ray-core On" of popupMenu
-        set ret to 0
-      else
-        click menu item "Turn v2ray-core Off" of popupMenu
-        set ret to 1
-      end if
-    ]])
-  end
-
-  local ok, ret = hs.osascript.applescript(script)
-  if ok then
-    if alert ~= nil and alert == true then
-      if ret == 0 then
-        hs.alert("V2Ray core loaded in \"V2rayU\"")
-      else
-        hs.alert("V2Ray core unloaded in \"V2rayU\"")
-      end
-    end
-  else
-    if alert ~= nil and alert ~= false then
+  local appUIObj = hs.axuielement.applicationElement(findApplication(bundleID))
+  local menu = getAXChildren(appUIObj, "AXMenuBar", -1, "AXMenuBarItem", 1,
+      "AXMenu", 1)
+  if menu == nil then
+    if alert then
       hs.alert("Error occurred while loading/unloading V2ray core in \"V2rayU\"")
     end
+    return false
   end
 
-  return ok
+  local set
+  local turnOnTitle = localizedString("Turn v2ray-core On", bundleID)
+  local turnOffTitle = localizedString("Turn v2ray-core Off", bundleID)
+  if enable == true then
+    local turnOn = getAXChildren(menu, "AXMenuItem", turnOnTitle)
+    if turnOn ~= nil then
+      turnOn:performAction("AXPress")
+      set = true
+    end
+  elseif enable == false then
+    local turnOff = getAXChildren(menu, "AXMenuItem", turnOffTitle)
+    if turnOff ~= nil then
+      turnOff:performAction("AXPress")
+      set = false
+    end
+  else
+    local turnOff = getAXChildren(menu, "AXMenuItem", turnOffTitle)
+    if turnOff ~= nil then
+      turnOff:performAction("AXPress")
+      set = false
+    else
+      local turnOn = getAXChildren(menu, "AXMenuItem", turnOnTitle)
+      turnOn:performAction("AXPress")
+      set = true
+    end
+  end
+
+  if enable == true or set then
+    local turnOff = getAXChildren(menu, "AXMenuItem", turnOffTitle)
+    if turnOff == nil then
+      if alert then
+        hs.alert("Error occurred while loading V2ray core in \"V2rayU\"")
+      end
+      return false
+    end
+  end
+
+  if alert then
+    if set then
+      hs.alert("V2Ray core loaded in \"V2rayU\"")
+    else
+      hs.alert("V2Ray core unloaded in \"V2rayU\"")
+    end
+  end
+
+  return true
 end
 
 -- toggle connect/disconnect VPN using `MonoCloud`(`MonoProxyMac`)
